5d3f447518c451172f39867c7a33a247f728dd59
KYs <amitkys59@gmail.com>
2025-09-27 20:03:33 +0530

    better and clean code for connection

--- go.mod
+++ go.mod
@@ -3,13 +3,18 @@ module github.com/amitkys/goCRUD
 go 1.25.0
 
 require (
+	github.com/gofiber/fiber/v3 v3.0.0-rc.1
+	github.com/joho/godotenv v1.5.1
+	go.mongodb.org/mongo-driver v1.17.4
+	go.mongodb.org/mongo-driver/v2 v2.3.0
+)
+
+require (
 	github.com/andybalholm/brotli v1.2.0 // indirect
-	github.com/gofiber/fiber/v3 v3.0.0-rc.1 // indirect
 	github.com/gofiber/schema v1.6.0 // indirect
 	github.com/gofiber/utils/v2 v2.0.0-rc.1 // indirect
 	github.com/golang/snappy v1.0.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -21,7 +26,6 @@ require (
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
-	go.mongodb.org/mongo-driver/v2 v2.3.0 // indirect
 	golang.org/x/crypto v0.42.0 // indirect
 	golang.org/x/net v0.44.0 // indirect
 	golang.org/x/sync v0.17.0 // indirect
--- main.go
+++ main.go
@@ -5,12 +5,21 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"time"
 
+	"github.com/gofiber/fiber/v3"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
+
+type Todo struct {
+  ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+  IsCompleted bool               `json:"isCompleted" bson:"isCompleted"`
+  Body        string             `json:"body" bson:"body"`
+}
+
+var coll *mongo.Collection
 
 func main() {
 	// Load environment variables
@@ -23,26 +32,34 @@ func main() {
 		log.Fatal("DB_URL is not set in .env")
 	}
 
-	// Create a context with timeout
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
+  client, err := mongo.Connect(options.Client().ApplyURI(dbURL))
+  if err != nil { 
+    panic(err)  
+  }
 
-	// Connect to MongoDB
-	clientOpts := options.Client().ApplyURI(dbURL)
-	client, err := mongo.Connect(clientOpts)
-	if err != nil {
-		log.Fatalf("Failed to connect to MongoDB: %v", err)
-	}
-	defer func() {
-		if err := client.Disconnect(ctx); err != nil {
-			log.Fatalf("Error disconnecting from MongoDB: %v", err)
-		}
-	}()
+  defer func () {
+    if err := client.Disconnect(context.TODO()); err != nil {
+      panic(err)
+    }
+  }()
 
-	// Ping the database
-	if err := client.Ping(ctx, nil); err != nil {
-		log.Fatalf("Could not ping MongoDB: %v", err)
-	}
+  coll = client.Database("crud_go").Collection("todo")
 
 	fmt.Println("Successfully connected to MongoDB")
+
+  app := fiber.New()
+
+  app.Get("/api/todos", getTodo)
+  // app.Post("/api/todos", addTodo)
+  // app.Patch("/api/todos/:id", updateTodo)
+  // app.Delete("/api/todos", deleteTodo)
+
+  port := os.Getenv("PORT")
+
+  log.Fatal(app.Listen(":"+port))
 }
+
+func getTodo(c fiber.Ctx) error {
+  
+}
+
