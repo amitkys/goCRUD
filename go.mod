5c195add9f0fd45b6137eebfb8576b9f19077dee
KYs <amitkys59@gmail.com>
2025-09-27 17:01:11 +0530

    mongodb connection successfully

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gofiber/fiber/v3 v3.0.0-rc.1 // indirect
 	github.com/gofiber/schema v1.6.0 // indirect
 	github.com/gofiber/utils/v2 v2.0.0-rc.1 // indirect
+	github.com/golang/snappy v1.0.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
@@ -16,8 +17,14 @@ require (
 	github.com/tinylib/msgp v1.4.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.66.0 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
+	go.mongodb.org/mongo-driver/v2 v2.3.0 // indirect
 	golang.org/x/crypto v0.42.0 // indirect
 	golang.org/x/net v0.44.0 // indirect
+	golang.org/x/sync v0.17.0 // indirect
 	golang.org/x/sys v0.36.0 // indirect
 	golang.org/x/text v0.29.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,101 +1,36 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 
-	"github.com/gofiber/fiber/v3"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
-type Todo struct {
-  Id int `json:"id"`
-  IsCompleted bool `json:"isCompleted"`
-  Body string `json:"body"`
-}
-
 func main() {
-  err := godotenv.Load(".env")
+	err := godotenv.Load(".env")
   if err != nil {
-    log.Fatal("Error loading .env file")
+    log.Fatal("error loading .env file")
   }
 
-  PORT := os.Getenv("PORT")
+  DB_URL := os.Getenv("DB_URL")
+  clientOption := options.Client().ApplyURI(DB_URL)
+  client, err := mongo.Connect(clientOption)
 
-  app := fiber.New()
+  if err != nil {
+    log.Fatal(err)
+  }
 
-  todos := []Todo{}
+  err = client.Ping(context.Background(), nil)
 
-  app.Get("/", func(c fiber.Ctx) error {
-    return c.Status(fiber.StatusOK).JSON(fiber.Map{"message": "Hello world"})
-  })
+  if err != nil {
+    log.Fatal(err)
+  }
 
-  // get all todo
-  app.Get("/api/getall", func(c fiber.Ctx) error {
-    return c.Status(200).JSON(todos)
-  })
-
-  // create todo
-  app.Post("/api/todo", func(c fiber.Ctx) error {
-    // get address of Todo struct litreal into 'todo' variable
-    todo := new(Todo) 
-
-    // parse incoming body request and bind value to creted todo struct litreal i.e. todo (with error handle)
-    if err := c.Bind().Body(todo); err != nil {
-      return err
-    }
-
-    // handle error when body is empty
-    if todo.Body == "" {
-      return c.Status(400).JSON(fiber.Map{"error": "Todo body should not be empty"})
-    }
-    // update todo id
-    todo.Id = len(todos) + 1
-    // append value of todo into 
-    todos = append(todos, *todo)
-
-    return c.Status(201).JSON(fiber.Map{
-      "message": "todo created successfully",
-      "new todo": todo,
-    })
-
-  })
-
-  // update todo
-  app.Patch("/api/todo/:id", func(c fiber.Ctx) error {
-    id := c.Params("id")
-
-    for i, todo := range todos {
-      if fmt.Sprint(todo.Id) == id {
-        todos[i].IsCompleted = true
-        return c.Status(200).JSON(todos[i])
-      }
-    }
-
-    return c.Status(400).JSON(fiber.Map{"error": "todo not found"})
-  })
-
-  // delete todo
-  app.Delete("/api/todo/:id", func(c fiber.Ctx) error {
-    id := c.Params("id")
-    
-    for i, todo := range todos {
-      if fmt.Sprint(todo.Id) == id {
-        todos = append(todos[:i], todos[i+1:]...)
-        return c.Status(200).JSON(fiber.Map{
-          "message": "todo deleted",
-          "id": id,
-        })    
-      }
-    }
-
-    return c.Status(400).JSON(fiber.Map{
-      "message": "todo not found",
-    })
-  }) 
-  // completed
-
-  log.Fatal(app.Listen(":"+PORT))
+  fmt.Println("connected to db")
 
 }
